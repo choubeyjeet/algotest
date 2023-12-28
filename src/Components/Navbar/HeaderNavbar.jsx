eb1bcd8231b7d4aef043ab9a183438a0b768133f
choubeyjeet <112739038+choubeyjeet@users.noreply.github.com>
2023-12-28 12:31:42 +0530

    Update HeaderNavbar.jsx

--- src/Components/Navbar/HeaderNavbar.jsx
+++ src/Components/Navbar/HeaderNavbar.jsx
@@ -3,7 +3,7 @@ import { Navbar, Nav } from 'rsuite';
 import HomeIcon from '@rsuite/icons/legacy/Home';
 import CogIcon from '@rsuite/icons/legacy/Cog';
 import Icon from "../Navbar/assets/img/stock.png";
-
+import { FaRegUserCircle, FaSignOutAlt } from "react-icons/fa";
 
 export default function HeaderNavbar() {
   const [activeKey, setActiveKey] = React.useState(null);
@@ -13,18 +13,34 @@ export default function HeaderNavbar() {
       <Navbar.Brand href="#"><img src={Icon} className='logo'/></Navbar.Brand>
       <Nav onSelect={setActiveKey} activeKey={activeKey}>
         <Nav.Item eventKey="1" icon={<HomeIcon />}>
-          Home
+        Live trade
         </Nav.Item>
-        <Nav.Item eventKey="2">News</Nav.Item>
-        <Nav.Item eventKey="3">Products</Nav.Item>
-        <Nav.Menu title="About">
+        <Nav.Item eventKey="2">Forward test</Nav.Item>
+        <Nav.Item eventKey="3">Portfolios</Nav.Item>
+        <Nav.Item eventKey="4">Signals</Nav.Item>
+        <Nav.Item eventKey="5">Charts</Nav.Item>
+        <Nav.Item eventKey="6">Simulator</Nav.Item>
+        <Nav.Item eventKey="7">Analyser</Nav.Item>
+        <Nav.Item eventKey="8">Pricing</Nav.Item>
+        
+        {/* <Nav.Menu title="About">
           <Nav.Item eventKey="4">Company</Nav.Item>
           <Nav.Item eventKey="5">Team</Nav.Item>
           <Nav.Item eventKey="6">Contact</Nav.Item>
-        </Nav.Menu>
+        </Nav.Menu> */}
       </Nav>
       <Nav pullRight>
-        <Nav.Item icon={<CogIcon />}>Settings</Nav.Item>
+      <Nav.Menu  icon={<FaRegUserCircle />} className='alignLeft'>
+          <Nav.Item eventKey="9">Profile</Nav.Item>
+          <Nav.Item eventKey="10">Margin Calculator</Nav.Item>
+          <Nav.Item eventKey="11">Explore AlgoTest</Nav.Item>
+          <Nav.Item eventKey="12">Broker Setup</Nav.Item>
+          <Nav.Item eventKey="13">Pricing</Nav.Item>
+          <Nav.Item eventKey="14">Blog</Nav.Item>
+          <Nav.Item eventKey="15">Community</Nav.Item>
+          <Nav.Item  icon={<FaSignOutAlt />} eventKey="16">Logout</Nav.Item>
+        </Nav.Menu>
+        
       </Nav>
     </Navbar>
     </>

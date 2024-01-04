51670ecc8dad73c45ce4442a4b05dc51a1678409
choubeyjeet <112739038+choubeyjeet@users.noreply.github.com>
2024-01-04 14:44:18 +0530

    Update HeaderNavbar.jsx

--- src/Components/Navbar/HeaderNavbar.jsx
+++ src/Components/Navbar/HeaderNavbar.jsx
@@ -40,7 +40,7 @@ useEffect(()=>{
   return (
     <>
        <Navbar >
-      <Navbar.Brand href="#"><img src={Icon} className='logo'/></Navbar.Brand>
+      <Navbar.Brand as={Link} to="/"><img src={Icon} className='logo'/></Navbar.Brand>
       <Nav onSelect={setActiveKey} activeKey={activeKey}>
         <Nav.Item eventKey="1" >
         Live trade
